9f298bdf422eee352fea5f722fbe28fbf4b50f6b
Naveen kumar <75235591+naveenkumar0106@users.noreply.github.com>
2020-11-30 10:32:05 +0530

    Add files via upload

---
+++ valid.js
@@ -0,0 +1,40 @@
+	//validation Code For inputs
+
+var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
+
+var email_error = document.getElementByID('email_error');
+var password_error = document.getElementByID('password_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', password_Verify);
+
+function validated(){
+	if (email.value.length < 9) {
+		email.style.border = "1px solid red;"
+		email_error.style.display = "block";
+		email.focus();
+		return false;
+	}
+	if (password.value.length < 9) {
+		password.style.border = "1px solid red;"
+		password_error.style.display = "block";
+		password.focus();
+		return false;
+	}
+
+}
+function email_Verify(){
+	if (email.value.length >=8){
+		email.style.border = "1px solid silver;"
+		email_error.style.display = "none";
+		return true;
+	}
+}
+function password_Verify(){
+	if (password.value.length >=5){
+		password.style.border = "1px solid silver;"
+		password_error.style.display = "none";
+		return true;
+	}
+}
